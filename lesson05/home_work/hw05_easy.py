feffbc3b0e5316c333d76c226f2b1bab04dd651d
daf-2000 <daf-2000@mail.ru>
2018-08-08 21:55:41 +0300

    Lesson_5_HW

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -2,9 +2,35 @@
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
+#Первый скрипт:
+import os
+for i in range(1,10):
+   dir_path = os.path.join(os.getcwd(), '{}_dir'.format(i))
+   try:
+      os.mkdir(dir_path)
+      print('директория dir_{} создана'.format(str(i)))
+   except FileExistsError:
+      print('директория {} уже существует'.format(dir_path))
+
+#Второй скрипт:
+import os
+for i in range(1, 10):
+    dir_path = os.path.join(os.getcwd(), '{}_dir'.format(str(i)))
+    if os.path.exists(dir_path):
+        os.rmdir(dir_path)
+        print('Директория {}_dir удалена'.format(str(i)))
+    else:
+        print('Такой папки не существует {}'.format(dir_path))
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
+import os
+for i in os.listdir('.'):
+    if os.path.isdir(i):
+        print(i)
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+import sys
+import shutil
+shutil.copy(sys.argv[0], sys.argv[0] + '.copy')
--- lesson05/home_work/hw05_hard.py
+++ lesson05/home_work/hw05_hard.py
@@ -13,3 +13,104 @@
 # Исходной директорией считать ту, в которой был запущен скрипт.
 
 # P.S. По возможности, сделайте кросс-платформенную реализацию.
+import os
+import sys
+import shutil
+import subprocess
+print('sys.argv = ', sys.argv)
+
+
+def print_help():
+    print("help - получение справки")
+    print("mkdir <dir_name> - создание директории")
+    print("ping - тестовый ключ")
+    print("cp <file_name> - создает копию указанного файла")
+    print("rm < file_name > - удаляет указанный файл")
+    print("cd <full_path or relative_path> - меняет текущую директорию на указанную")
+    print("ls - отображение полного пути текущей директории")
+
+def rm():
+    if not dir_name:
+        print("Необходимо указать имя файла вторым параметром")
+        return
+    file_path = os.path.join(os.getcwd(), dir_name)
+    if os.path.isfile(file_path):
+        if input('Вы уверены? Y/N: ') in ('Y', 'y'):
+            os.remove(file_path)
+            print('Файл {} удален'.format(file_path))
+        else:
+            print('Операция отменена')
+    else:
+        print('Такаго файла не существует')
+
+def cd():
+    path = dir_name
+    if os.path.isdir(dir_name):
+        if os.name == 'nt':
+            subprocess.call(['cmd', '/k', 'cd {}'.format(dir_name)])
+        if os.name =='posix':
+            subprocess.call(['cd {}'.format(dir_name)])
+            os.chdir(dir_name)
+    else:
+        print('Такого пути не существует')
+def ls():
+    os.getcwd()
+
+def cp():
+    if not dir_name:
+        print("Необходимо указать имя файла вторым параметром")
+        return
+    file_path = os.path.join(os.getcwd(), dir_name)
+    if os.path.isfile(file_path):
+        if os.path.isfile(file_path + '.copy'):
+            print('Копия уже существует')
+            return
+        else:
+            shutil.copy(file_path, file_path + '.copy')
+            print('Создан файл {}'.format(file_path + '.copy'))
+    else:
+        print('Такого файла не существует')
+
+def make_dir():
+    if not dir_name:
+        print("Необходимо указать имя директории вторым параметром")
+        return
+    dir_path = os.path.join(os.getcwd(), dir_name)
+    try:
+        os.mkdir(dir_path)
+        print('директория {} создана'.format(dir_name))
+    except FileExistsError:
+        print('директория {} уже существует'.format(dir_name))
+
+
+def ping():
+    print("pong")
+
+do = {
+    "help": print_help,
+    "mkdir": make_dir,
+    "ping": ping,
+    "cp": cp,
+    "rm": rm,
+    "cd": cd,
+    "ls": ls
+}
+
+try:
+    dir_name = sys.argv[2]
+except IndexError:
+    dir_name = None
+
+try:
+    key = sys.argv[1]
+except IndexError:
+    key = None
+
+
+if key:
+    if do.get(key):
+        do[key]()
+    else:
+        print("Задан неверный ключ")
+        print("Укажите ключ help для получения справки")
+print(os.getcwd())
